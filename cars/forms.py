179c6ea1e6d1caef96d915e36b3e8d64361ee274
leonardorz5 <leonardo.limal15@hotmail.com>
2024-06-21 11:46:03 -0300

    primeiro commit

---
+++ cars/forms.py
@@ -0,0 +1,13 @@
+from django import forms 
+from cars.models import Car
+
+class CarModelForm(forms.ModelForm):   
+    class Meta:
+        model = Car
+        fields = '__all__'
+        
+    def clean_value(self):
+        value = self.cleaned_data.get('value')
+        if value < 20000:
+            self.add_error('value', 'valor mínimo permitido é de R$20.000')
+        return value
---
+++ cars/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from cars.models import Car
+from cars.forms import CarModelForm
+from django.views.generic import ListView,DetailView, CreateView
+
+
+class CarListView(ListView):
+    model = Car
+    template_name = 'cars.html'
+    context_object_name = 'cars'
+    
+    def get_queryset(self):
+        cars = super().get_queryset().order_by('model')
+        search = self.request.GET.get('search')
+        if search:
+            cars= cars.filter(model__icontains=search)
+        return cars
+    
+class NewCarCreateView(CreateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'new_car.html'
+    success_url = '/cars/'
+    
+class CarDetailView(DetailView):
+    model = Car
+    template_name = 'car_detail.html'
